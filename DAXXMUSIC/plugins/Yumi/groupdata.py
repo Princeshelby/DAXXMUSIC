9431faa026678781ed214f3582be8d4d62c9bebd
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2023-12-29 14:26:32 +0530

    Update groupdata.py

--- DAXXMUSIC/plugins/Yumi/groupdata.py
+++ DAXXMUSIC/plugins/Yumi/groupdata.py
@@ -46,7 +46,7 @@ async def instatus(app, message):
 ๏ ᴘʀᴇᴍɪᴜᴍ ᴜsᴇʀs ➠ {premium_acc} 
 ๏ ᴛɪᴍᴇ ᴛᴀᴋᴇɴ ➠ {timelog} 
 
-๏ ᴍᴀᴅᴇ ʙʏ ➠ ʀᴏʏ-ᴇᴅɪᴛx **""")
+๏ ᴍᴀᴅᴇ ʙʏ ➛ ᴘʀɪɴᴄᴇ ᥫᩣ""")
     else:
         sent_message = await message.reply_text("ONLY ADMINS CAN USE THIS !")
         await sleep(5)
